2d71d9fd1629738890d5d884013196136eb70251
maki1007 <maki860710@gmail.com>
2024-05-02 12:07:27 +0300

    финально отредактировано

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -3,7 +3,7 @@ from .locators import BasketPageLocators
 
 class BasketPage(BasePage):
     def empty_basket(self):
-        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET), \
+        assert self.is_not_element_present(*BasketPageLocators.EMPTY_BASKET), \
         "Basket is not empty, but it should be"
         assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), \
         "Empty basket message is not presented, but it should be"
